41061c6163c15033c762dd837a67aa1d3bbd657b
Nakshatra0707 <83689058+Nakshatra0707@users.noreply.github.com>
2021-07-28 19:45:50 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function summer(){
-    localStorage.setItem("season", "summer")
-    window.location = "time.html"
-}
-
-function winter(){
-    localStorage.setItem("season", "winter")
-    window.location = "time.html"
-}
