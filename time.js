a9ae26552b900dbd27983cd21eae6fdd92eee52f
Nakshatra0707 <83689058+Nakshatra0707@users.noreply.github.com>
2021-07-28 19:46:57 +0530

    Delete time.js

--- time.js
+++
@@ -1,7 +0,0 @@
-function bf(){
-    time = localStorage.setItem("time", "breakfast")
-    season = localStorage.getItem("season")
-    if (season == "summer"){
-        window.location = "summer-breakfast.html"
-    }
-}
